0085101935393e9dfe493462cbc5c70b47ed6744
Daniel Mathew <94301704+Dantheman2606@users.noreply.github.com>
2026-02-09 23:49:55 +0530

    Add RabbitMQ spike with Send/Recv examples

---
+++ spikes/rabbitmq-basic/src/main/java/com/daniel/rabbitmq_basics/Recv.java
@@ -0,0 +1,30 @@
+package com.daniel.rabbitmq_basics;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Recv {
+    private static final String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        System.out.println("Waiting for msgs, Press Ctrl + C to quit");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+
+            System.out.println("Received msg: " + message);
+        };
+
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+    }
+}
